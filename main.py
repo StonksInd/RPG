43948c38f7604ff002b6bb92c70eec276ae56222
Nath <nathlej41@gmail.com>
2023-12-07 14:00:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,3 @@ armor_thornmail = Armor("Thornmail", 60, 25, 0)
 francois = Mage("jean", no_armor, no_weapon, 100)
 print(francois.weapon.dammage) 
       
-    bonjour
-    kikou
-;,j
-aert
-    
-    
-    zroutyiugytirfeh
-    azerty
